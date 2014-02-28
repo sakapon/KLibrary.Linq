679ecc99f2011d821504e2392948f86223cb4407
Keiho Sakapon <github@saka-pon.net>
2014-02-28 17:21:09 +0900

    Update project settings.

--- KLibrary2_1/Linq/Properties/AssemblyInfo.cs
+++ KLibrary2_1/Linq/Properties/AssemblyInfo.cs
@@ -1,18 +1,17 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("Linq")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Linq")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("KLibrary.Linq")]
+[assembly: AssemblyDescription("The library for LINQ.")]
+[assembly: AssemblyMetadata("ProjectUrl", "https://github.com/sakapon/KLibrary-Linq")]
+[assembly: AssemblyMetadata("LicenseUrl", "https://github.com/sakapon/KLibrary-Linq/blob/master/LICENSE")]
+[assembly: AssemblyMetadata("Tags", "LINQ")]
+[assembly: AssemblyMetadata("ReleaseNotes", "")]
 
 // ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
 // 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
@@ -22,15 +21,8 @@ using System.Runtime.InteropServices;
 // 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
 [assembly: Guid("8feab172-f632-489e-be62-2dad5e106a9b")]
 
-// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
-// 既定値にすることができます:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
+
+// Retrieve the full public key from a strong-named key (.snk) file.
+// http://msdn.microsoft.com/library/system.runtime.compilerservices.internalsvisibletoattribute.internalsvisibletoattribute.aspx
+[assembly: InternalsVisibleTo("UnitTest, PublicKey=00240000048000009400000006020000002400005253413100040000010001008faa67fc5a4699eb3d9ede2be977efc6bcc4c019ecaf43e746b506eb48f2f1fa27702d41c116b2659cd0d4f34103a72d9aec77e8ff732a0555dfc6a444a2b2072b58a82fd9b7300b02aeef8913fccf795c3aa831c28e027477ad087a6814fba7636a79b904490e04e84615bf8879941deb62a93bf159298e1a9397f3bf7bb6ad")]
