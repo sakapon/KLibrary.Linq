032b5096426a57b6407d9782feef046fb10fbbac
Keiho Sakapon <github@saka-pon.net>
2014-02-28 16:38:18 +0900

    Add tool to create package.

---
+++ Tools/CreatePackage.ps1
@@ -0,0 +1,10 @@
+﻿$msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+
+cd ..\KLibrary2_1
+Invoke-Expression ($msbuild + " KLibrary2_1.sln /p:Configuration=Release /t:Clean")
+Invoke-Expression ($msbuild + " KLibrary2_1.sln /p:Configuration=Release /t:Rebuild")
+
+cd .\Linq
+.\NuGetPackup.exe
+
+move *.nupkg ..\..\Published -Force
