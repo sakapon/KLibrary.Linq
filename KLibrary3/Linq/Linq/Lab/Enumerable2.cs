2ce9bbe70918e5ae30ae6781bce82aa6be360bfa
Keiho Sakapon <git@saka-pon.net>
2016-12-11 00:44:06 +0900

    Add methods

---
+++ KLibrary3/Linq/Linq/Lab/Enumerable2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KLibrary.Linq.Lab
+{
+    public static class Enumerable2
+    {
+        public static IEnumerable<TSource> CopyTo<TSource>(this IEnumerable<TSource> source, TSource[] array)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (array == null) throw new ArgumentNullException("array");
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                for (var i = 0; i < array.Length && enumerator.MoveNext(); i++)
+                    array[i] = enumerator.Current;
+            }
+
+            return source;
+        }
+    }
+}
