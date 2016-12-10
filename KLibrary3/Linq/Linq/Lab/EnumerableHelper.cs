98144db33f169c51758d6cb4072820b01199efa5
Keiho Sakapon <git@saka-pon.net>
2016-12-10 20:15:44 +0900

    Add methods

---
+++ KLibrary3/Linq/Linq/Lab/EnumerableHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KLibrary.Linq.Lab
+{
+    public static class EnumerableHelper
+    {
+        public static IEnumerable<int> Range2(int minValue, int maxValue)
+        {
+            for (var i = minValue; i <= maxValue; i++)
+                yield return i;
+        }
+    }
+}
