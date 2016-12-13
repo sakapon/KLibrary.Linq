f3008c4b9289df7237b57b55d52d4c905ecc7e82
Keiho Sakapon <git@saka-pon.net>
2016-12-13 14:40:04 +0900

    Add methods

--- KLibrary3/Linq/Linq/Lab/EnumerablePipelines.cs
+++ KLibrary3/Linq/Linq/Lab/EnumerablePipelines.cs
@@ -19,5 +19,45 @@ namespace KLibrary.Linq.Lab
 
             return source;
         }
+
+        public static TSource FirstToMin<TSource>(this IEnumerable<TSource> source, Func<TSource, double> valueSelector)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            var o = source
+                .Select(e => new { e, v = valueSelector(e) })
+                .Aggregate((o1, o2) => o1.v <= o2.v ? o1 : o2);
+            return o.e;
+        }
+
+        public static TSource FirstToMax<TSource>(this IEnumerable<TSource> source, Func<TSource, double> valueSelector)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            var o = source
+                .Select(e => new { e, v = valueSelector(e) })
+                .Aggregate((o1, o2) => o1.v >= o2.v ? o1 : o2);
+            return o.e;
+        }
+
+        public static TSource LastToMin<TSource>(this IEnumerable<TSource> source, Func<TSource, double> valueSelector)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            var o = source
+                .Select(e => new { e, v = valueSelector(e) })
+                .Aggregate((o1, o2) => o1.v < o2.v ? o1 : o2);
+            return o.e;
+        }
+
+        public static TSource LastToMax<TSource>(this IEnumerable<TSource> source, Func<TSource, double> valueSelector)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            var o = source
+                .Select(e => new { e, v = valueSelector(e) })
+                .Aggregate((o1, o2) => o1.v > o2.v ? o1 : o2);
+            return o.e;
+        }
     }
 }
