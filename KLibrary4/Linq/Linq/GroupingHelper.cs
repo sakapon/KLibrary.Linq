e386c931a886069281a81fde434f8586512f7e96
Keiho Sakapon <git@saka-pon.net>
2019-01-16 18:45:21 +0900

    Add classes

---
+++ KLibrary4/Linq/Linq/GroupingHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KLibrary.Linq
+{
+    public static class GroupingHelper
+    {
+        public static IEnumerable<IGrouping<TKey, TSource>> GroupBySequentially<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (keySelector == null) throw new ArgumentNullException("keySelector");
+
+            var queue = new Queue<TSource>();
+            var currentKey = default(TKey);
+            var comparer = EqualityComparer<TKey>.Default;
+
+            foreach (var item in source)
+            {
+                var key = keySelector(item);
+
+                if (!comparer.Equals(key, currentKey))
+                {
+                    if (queue.Count != 0)
+                    {
+                        yield return new Grouping<TKey, TSource>(currentKey, queue.ToArray());
+                        queue.Clear();
+                    }
+                    currentKey = key;
+                }
+                queue.Enqueue(item);
+            }
+
+            if (queue.Count != 0)
+            {
+                yield return new Grouping<TKey, TSource>(currentKey, queue.ToArray());
+                queue.Clear();
+            }
+        }
+    }
+
+    public class Grouping<TKey, TElement> : IGrouping<TKey, TElement>
+    {
+        public TKey Key { get; }
+        protected IEnumerable<TElement> Values { get; }
+
+        public Grouping(TKey key, IEnumerable<TElement> values)
+        {
+            Key = key;
+            Values = values;
+        }
+
+        public IEnumerator<TElement> GetEnumerator()
+        {
+            return Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
