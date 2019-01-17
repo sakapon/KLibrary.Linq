81b71c80c408b0713570ab56b29536e70fabb920
Keiho Sakapon <git@saka-pon.net>
2019-01-17 14:32:57 +0900

    Add test

---
+++ KLibrary4/UnitTest/Linq/GroupingHelperTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KLibrary.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Linq
+{
+    [TestClass]
+    public class GroupingHelperTest
+    {
+        [TestMethod]
+        public void GroupBySequentially_1()
+        {
+            var target = Enumerable.Range(0, 9)
+                .GroupBySequentially(i => i % 4 == 0)
+                .ToArray();
+
+            CollectionAssert.AreEqual(new[] { true, false, true, false, true }, target.Select(g => g.Key).ToArray());
+            CollectionAssert.AreEqual(new[] { 1, 3, 1, 3, 1 }, target.Select(g => g.ToArray().Length).ToArray());
+        }
+
+        [TestMethod]
+        public void GroupBySequentially_2()
+        {
+            var target = Enumerable.Range(1, 10)
+                .GroupBySequentially(i => i % 4 == 0)
+                .ToArray();
+
+            CollectionAssert.AreEqual(new[] { false, true, false, true, false }, target.Select(g => g.Key).ToArray());
+            CollectionAssert.AreEqual(new[] { 3, 1, 3, 1, 2 }, target.Select(g => g.ToArray().Length).ToArray());
+        }
+    }
+}
