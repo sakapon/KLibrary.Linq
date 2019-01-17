ac17c53cf53326337e38e6a57d1053c3a73844bd
Keiho Sakapon <git@saka-pon.net>
2019-01-17 17:53:58 +0900

    Add test

---
+++ KLibrary4/UnitTest/Linq/CompositionTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KLibrary.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Linq
+{
+    [TestClass]
+    public class CompositionTest
+    {
+        [TestMethod]
+        public void ZipForShort_1()
+        {
+            var length1 = 9;
+            var length2 = 10;
+            var range1 = Enumerable.Range(1, length1);
+            var range2 = Enumerable.Range(1, length2);
+
+            var actual1 = range1.ZipForShort(range2).ToArray();
+            Assert.AreEqual(length1, actual1.Length);
+
+            var actual2 = range2.ZipForShort(range1).ToArray();
+            Assert.AreEqual(length1, actual2.Length);
+        }
+
+        [TestMethod]
+        public void ZipForLong_1()
+        {
+            var length1 = 9;
+            var length2 = 10;
+            var range1 = Enumerable.Range(1, length1);
+            var range2 = Enumerable.Range(1, length2);
+
+            var actual1 = range1.ZipForLong(range2).ToArray();
+            Assert.AreEqual(length2, actual1.Length);
+            Assert.AreEqual(0, actual1[length2 - 1].Item1);
+
+            var actual2 = range2.ZipForLong(range1).ToArray();
+            Assert.AreEqual(length2, actual2.Length);
+            Assert.AreEqual(0, actual2[length2 - 1].Item2);
+        }
+
+        [TestMethod]
+        public void ZipForFirst_1()
+        {
+            var length1 = 9;
+            var length2 = 10;
+            var range1 = Enumerable.Range(1, length1);
+            var range2 = Enumerable.Range(1, length2);
+
+            var actual1 = range1.ZipForFirst(range2).ToArray();
+            Assert.AreEqual(length1, actual1.Length);
+
+            var actual2 = range2.ZipForFirst(range1).ToArray();
+            Assert.AreEqual(length2, actual2.Length);
+            Assert.AreEqual(0, actual2[length2 - 1].Item2);
+        }
+    }
+}
