602ca921460e90a211c57b647bc8e34bf8c32cef
Keiho Sakapon <git@saka-pon.net>
2019-01-21 12:50:44 +0900

    Add method

--- KLibrary4/Linq/Linq/Lab/Enumerable2.cs
+++ KLibrary4/Linq/Linq/Lab/Enumerable2.cs
@@ -6,6 +6,24 @@ namespace KLibrary.Linq.Lab
 {
     public static class Enumerable2
     {
+        public static IEnumerable<int> Range(int start = 0, int? count = null, int step = 1)
+        {
+            if (count.HasValue)
+            {
+                var n = start;
+                for (var i = 0; i < count; i++)
+                {
+                    yield return n;
+                    n += step;
+                }
+            }
+            else
+            {
+                for (var i = start; ; i += step)
+                    yield return i;
+            }
+        }
+
         public static IEnumerable<int> Range2(int minValue, int maxValue)
         {
             for (var i = minValue; i <= maxValue; i++)
---
+++ KLibrary4/UnitTest/Linq/Lab/Enumerable2Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static KLibrary.Linq.Lab.Enumerable2;
+
+namespace UnitTest.Linq.Lab
+{
+    [TestClass]
+    public class Enumerable2Test
+    {
+        [TestMethod]
+        public void Range_1()
+        {
+            CollectionAssert.AreEqual(Enumerable.Range(0, 10).ToArray(), Range().Take(10).ToArray());
+            CollectionAssert.AreEqual(Enumerable.Range(3, 10).ToArray(), Range(3).Take(10).ToArray());
+            CollectionAssert.AreEqual(Enumerable.Range(0, 10).ToArray(), Range(0, 10).ToArray());
+            CollectionAssert.AreEqual(Enumerable.Range(3, 10).ToArray(), Range(3, 10).ToArray());
+            CollectionAssert.AreEqual(Enumerable.Range(0, 10).Select(i => 2 * i).ToArray(), Range(0, null, 2).Take(10).ToArray());
+            CollectionAssert.AreEqual(Enumerable.Range(0, 10).Select(i => 2 * i + 3).ToArray(), Range(3, 10, 2).ToArray());
+        }
+    }
+}
