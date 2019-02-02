b6c8b23d022519b41391176dceceb6f20b52063c
Keiho Sakapon <git@saka-pon.net>
2019-02-02 16:23:23 +0900

    Copy test code

--- KLibrary4/UnitTest/Linq/PipelinesTest.cs
+++ KLibrary4/UnitTest/Linq/PipelinesTest.cs
@@ -57,6 +57,16 @@ namespace UnitTest.Linq
         {
             var sum = 0;
             Enumerable.Range(1, 10)
+                .Do(i => sum += i)
+                .Execute();
+            Assert.AreEqual(55, sum);
+        }
+
+        [TestMethod]
+        public void Execute_2()
+        {
+            var sum = 0;
+            Enumerable.Range(1, 10)
                 .Execute(i => sum += i);
             Assert.AreEqual(55, sum);
 
@@ -65,6 +75,67 @@ namespace UnitTest.Linq
             Enumerable.Range(1, 10)
                 .Execute(target.Add);
             CollectionAssert.AreEqual(expected, target);
+        }
+
+        [TestMethod]
+        public void Prepend_1()
+        {
+            var result = Enumerable.Range(0, 10)
+                .Prepend(-1)
+                .ToArray();
+            var expected = Enumerable.Range(-1, 11)
+                .ToArray();
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Append_1()
+        {
+            var result = Enumerable.Range(0, 10)
+                .Append(10)
+                .ToArray();
+            var expected = Enumerable.Range(0, 11)
+                .ToArray();
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Distinct_1()
+        {
+            var result = Enumerable.Range(1, 10)
+                .Select(x => x * x)
+                .Distinct(x => x % 3)
+                .ToArray();
+
+            CollectionAssert.AreEqual(new[] { 1, 9 }, result);
+        }
+
+        [TestMethod]
+        public void Segment_1()
+        {
+            var result = Enumerable.Range(0, 9)
+                .Segment(3)
+                .ToArray();
+
+            Assert.AreEqual(3, result.Length);
+            CollectionAssert.AreEqual(new[] { 0, 1, 2 }, result[0]);
+            CollectionAssert.AreEqual(new[] { 3, 4, 5 }, result[1]);
+            CollectionAssert.AreEqual(new[] { 6, 7, 8 }, result[2]);
+        }
+
+        [TestMethod]
+        public void Segment_2()
+        {
+            var result = Enumerable.Range(0, 8)
+                .Segment(3)
+                .ToArray();
+
+            Assert.AreEqual(3, result.Length);
+            CollectionAssert.AreEqual(new[] { 0, 1, 2 }, result[0]);
+            CollectionAssert.AreEqual(new[] { 3, 4, 5 }, result[1]);
+            CollectionAssert.AreEqual(new[] { 6, 7 }, result[2]);
         }
 
         [TestMethod]
