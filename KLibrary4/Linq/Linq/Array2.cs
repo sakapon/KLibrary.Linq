5c65ab5467757af5923e643da7a99afbde834512
Keiho Sakapon <git@saka-pon.net>
2019-02-08 16:10:16 +0900

    Add classes

---
+++ KLibrary4/Linq/Linq/Array2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace KLibrary.Linq
+{
+    public static class Array2
+    {
+        public static T[] Subarray<T>(this T[] source, int start, int count)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            var a = new T[count];
+            Array.Copy(source, start, a, 0, count);
+            return a;
+        }
+
+        public static Tuple<T[], T[]> Split<T>(this T[] source, int index)
+        {
+            var a1 = source.Subarray(0, index);
+            var a2 = source.Subarray(index, source.Length - index);
+            return Tuple.Create(a1, a2);
+        }
+
+        public static bool ArrayEqual<T>(this T[] first, T[] second)
+        {
+            if (first == null) throw new ArgumentNullException(nameof(first));
+            if (second == null) throw new ArgumentNullException(nameof(second));
+
+            if (first.Length != second.Length) return false;
+            for (var i = 0; i < first.Length; i++)
+                if (!Equals(first[i], second[i])) return false;
+            return true;
+        }
+    }
+}
